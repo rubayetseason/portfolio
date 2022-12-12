a66e7e4da1e28107962f16abb46079d588ea56d4
rubayet-islam <season.ss2001@gmail.com>
2022-12-12 19:28:15 +0600

    minor adjustment

--- src/Components/Shared/Footer.js
+++ src/Components/Shared/Footer.js
@@ -5,29 +5,29 @@ const Footer = () => {
   return (
     <footer className="footer mt-10 footer-center p-10 bg-secondary text-base-content rounded">
       <div className="grid grid-flow-col gap-4">
-        <a href="#homeid" className="link link-hover">
+        <a href="#homeid" className="link link-hover hover:text-primary">
           Home
         </a>
-        <a href="#serviceid" className="link link-hover">
+        <a href="#serviceid" className="link link-hover hover:text-primary">
           Services
         </a>
-        <a href="#projectsId" className="link link-hover">
+        <a href="#projectsId" className="link link-hover hover:text-primary">
           Projects
         </a>
-        <a href="#contactId" className="link link-hover">
+        <a href="#contactId" className="link link-hover hover:text-primary">
           Contact
         </a>
       </div>
       <div>
         <div className="grid grid-flow-col gap-4">
           <a href="https://www.linkedin.com/in/rubayet-islam2001/">
-            <FiLinkedin className="text-2xl" />
+            <FiLinkedin className="text-2xl hover:text-primary" />
           </a>
           <a href="https://github.com/rubayetseason">
-            <FiGithub className="text-2xl" />
+            <FiGithub className="text-2xl hover:text-primary" />
           </a>
           <a href="https://www.facebook.com/rubayet.islam.sizn">
-            <FiFacebook className="text-2xl" />
+            <FiFacebook className="text-2xl hover:text-primary" />
           </a>
         </div>
       </div>
